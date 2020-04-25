7b75e7b544e7f75912ff5e0229a6b10f6301c905
Rickard Andersson <gonz@severnatazvezda.com>
2020-04-25 11:05:42 +0300

    output union examples

--- src/main.zig
+++ src/main.zig
@@ -7,11 +7,23 @@ pub const haskell = @import("./haskell.zig");
 
 pub fn main() anyerror!void {
     const typescript_struct_output = typescript.typedefinitionToString(types.BasicStruct);
-    std.debug.warn("TypeScript:\n{}\n", .{typescript_struct_output});
+    const typescript_union_output = typescript.typedefinitionToString(types.BasicUnion);
+    std.debug.warn(
+        "TypeScript:\n{}\n{}\n",
+        .{ typescript_struct_output, typescript_union_output },
+    );
 
     const purescript_struct_output = purescript.typedefinitionToString(types.BasicStruct);
-    std.debug.warn("\nPureScript:\n{}\n", .{purescript_struct_output});
+    const purescript_union_output = purescript.typedefinitionToString(types.BasicUnion);
+    std.debug.warn(
+        "\nPureScript:\n{}\n{}\n",
+        .{ purescript_struct_output, purescript_union_output },
+    );
 
     const haskell_struct_output = haskell.typedefinitionToString(types.BasicStruct);
-    std.debug.warn("\nHaskell:\n{}\n", .{haskell_struct_output});
+    const haskell_union_output = haskell.typedefinitionToString(types.BasicUnion);
+    std.debug.warn(
+        "\nHaskell:\n{}\n{}\n",
+        .{ haskell_struct_output, haskell_union_output },
+    );
 }
