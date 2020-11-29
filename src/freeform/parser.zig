72b3a481df1f31025a4d0916a706e57b469e7e34
Rickard Andersson <gonz@severnatazvezda.com>
2020-11-29 10:33:36 +0200

    get started on `parser.zig`
    A basic outline for now, trying to establish what the definitions look
    like/contain.

---
+++ src/freeform/parser.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+const debug = std.debug;
+const mem = std.mem;
+const heap = std.heap;
+const testing = std.testing;
+const fmt = std.fmt;
+
+const type_examples = @import("./type_examples.zig");
+
+pub const Definitions = union(enum) {
+    structure: Structure,
+    @"union": Union,
+};
+
+pub const Structure = union(enum) {
+    plain_structure: PlainStructure,
+    generic_structure: GenericStructure,
+};
+
+pub const PlainStructure = struct {
+    name: []const u8,
+    fields: []Field,
+};
+
+pub const GenericStructure = struct {
+    name: []const u8,
+    fields: []Field,
+    open_names: []OpenName,
+};
+
+pub const Union = union(enum) {
+    plain_union: PlainUnion,
+    generic_union: GenericUnion,
+};
