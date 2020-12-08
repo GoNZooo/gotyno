3dd9d21ee41c28583481d63060ec0e9ba48f219b
Rickard Andersson <gonz@severnatazvezda.com>
2020-12-08 20:09:14 +0200

    rename binary to `gotyno`

--- build.zig
+++ build.zig
@@ -12,7 +12,7 @@ const test_files = [_][]const u8{
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const target = b.standardTargetOptions(.{});
-    const exe = b.addExecutable("type-translator", "src/main.zig");
+    const exe = b.addExecutable("gotyno", "src/main.zig");
     exe.setBuildMode(mode);
     exe.setTarget(target);
     exe.install();
