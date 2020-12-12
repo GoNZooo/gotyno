5049c123a821ab220028956940aec561b680918a
Rickard Andersson <gonz@severnatazvezda.com>
2020-12-12 11:54:56 +0200

    update `simple-validation-tools` & rebuild example

--- test_files/basic.ts
+++ test_files/basic.ts
@@ -20,14 +20,15 @@ export type Person = {
     hobbies: string[];
     last_fifteen_comments: string[];
     recruiter: Recruiter;
+    spouse: Maybe<Person>;
 };
 
 export function isPerson(value: unknown): value is Person {
-    return svt.isInterface<Person>(value, {name: svt.isString, age: svt.isNumber, efficiency: svt.isNumber, on_vacation: svt.isBoolean, hobbies: svt.arrayOf(svt.isString), last_fifteen_comments: svt.arrayOf(svt.isString), recruiter: isRecruiter});
+    return svt.isInterface<Person>(value, {name: svt.isString, age: svt.isNumber, efficiency: svt.isNumber, on_vacation: svt.isBoolean, hobbies: svt.arrayOf(svt.isString), last_fifteen_comments: svt.arrayOf(svt.isString), recruiter: isRecruiter, spouse: isMaybe(isPerson)});
 }
 
 export function validatePerson(value: unknown): svt.ValidationResult<Person> {
-    return svt.validate<Person>(value, {name: svt.validateString, age: svt.validateNumber, efficiency: svt.validateNumber, on_vacation: svt.validateBoolean, hobbies: svt.validateArray(svt.validateString), last_fifteen_comments: svt.validateArray(svt.validateString), recruiter: validateRecruiter});
+    return svt.validate<Person>(value, {name: svt.validateString, age: svt.validateNumber, efficiency: svt.validateNumber, on_vacation: svt.validateBoolean, hobbies: svt.validateArray(svt.validateString), last_fifteen_comments: svt.validateArray(svt.validateString), recruiter: validateRecruiter, spouse: validateMaybe(validatePerson)});
 }
 
 export type LogInData = {
@@ -118,6 +119,10 @@ export function SetEmails(data: Email[]): SetEmails {
     return {type: "SetEmails", data};
 }
 
+export function isEvent(value: unknown): value is Event {
+    return [isLogIn, isLogOut, isJoinChannels, isSetEmails].some((typePredicate) => typePredicate(value));
+}
+
 export function isLogIn(value: unknown): value is LogIn {
     return svt.isInterface<LogIn>(value, {type: "LogIn", data: isLogInData});
 }
@@ -134,6 +139,10 @@ export function isSetEmails(value: unknown): value is SetEmails {
     return svt.isInterface<SetEmails>(value, {type: "SetEmails", data: svt.arrayOf(isEmail)});
 }
 
+export function validateEvent(value: unknown): svt.ValidationResult<Event> {
+    return svt.validateOneOf<Event>(value, [validateLogIn, validateLogOut, validateJoinChannels, validateSetEmails]);
+}
+
 export function validateLogIn(value: unknown): svt.ValidationResult<LogIn> {
     return svt.validate<LogIn>(value, {type: "LogIn", data: validateLogInData});
 }
@@ -148,4 +157,111 @@ export function validateJoinChannels(value: unknown): svt.ValidationResult<JoinC
 
 export function validateSetEmails(value: unknown): svt.ValidationResult<SetEmails> {
     return svt.validate<SetEmails>(value, {type: "SetEmails", data: svt.validateArray(validateEmail)});
+}
+
+export type Maybe<T> = Nothing | Just<T>;
+
+export type Nothing = {
+    type: "Nothing";
+};
+
+export type Just<T> = {
+    type: "Just";
+    data: T;
+};
+
+export function Nothing(): Nothing {
+    return {type: "Nothing"};
+}
+
+export function Just<T>(data: T): Just<T> {
+    return {type: "Just", data};
+}
+
+export function isMaybe<T>(isT: svt.TypePredicate<T>): svt.TypePredicate<Maybe<T>> {
+    return function isMaybeT(value: unknown): value is Maybe<T> {
+        return [isNothing, isJust(isT)].some((typePredicate) => typePredicate(value));
+    };
+}
+
+export function isNothing(value: unknown): value is Nothing {
+    return svt.isInterface<Nothing>(value, {type: "Nothing"});
+}
+
+export function isJust<T>(isT: svt.TypePredicate<T>): svt.TypePredicate<Just<T>> {
+    return function isJustT(value: unknown): value is Just<T> {
+        return svt.isInterface<Just<T>>(value, {type: "Just", data: isT});
+    };
+}
+
+export function validateMaybe<T>(validateT: svt.Validator<T>): svt.Validator<Maybe<T>> {
+    return function validateMaybeT(value: unknown): svt.ValidationResult<Maybe<T>> {
+        return svt.validateOneOf<Maybe<T>>(value, [validateNothing, validateJust(validateT)]);
+    };
+}
+
+export function validateNothing(value: unknown): svt.ValidationResult<Nothing> {
+    return svt.validate<Nothing>(value, {type: "Nothing"});
+}
+
+export function validateJust<T>(validateT: svt.Validator<T>): svt.Validator<Just<T>> {
+    return function validateJustT(value: unknown): svt.ValidationResult<Just<T>> {
+        return svt.validate<Just<T>>(value, {type: "Just", data: validateT});
+    };
+}
+
+export type Either<L, R> = Left<L> | Right<R>;
+
+export type Left<L> = {
+    type: "Left";
+    data: L;
+};
+
+export type Right<R> = {
+    type: "Right";
+    data: R;
+};
+
+export function Left<L>(data: L): Left<L> {
+    return {type: "Left", data};
+}
+
+export function Right<R>(data: R): Right<R> {
+    return {type: "Right", data};
+}
+
+export function isEither<L, R>(isL: svt.TypePredicate<L>, isR: svt.TypePredicate<R>): svt.TypePredicate<Either<L, R>> {
+    return function isEitherLR(value: unknown): value is Either<L, R> {
+        return [isLeft(isL), isRight(isR)].some((typePredicate) => typePredicate(value));
+    };
+}
+
+export function isLeft<L>(isL: svt.TypePredicate<L>): svt.TypePredicate<Left<L>> {
+    return function isLeftL(value: unknown): value is Left<L> {
+        return svt.isInterface<Left<L>>(value, {type: "Left", data: isL});
+    };
+}
+
+export function isRight<R>(isR: svt.TypePredicate<R>): svt.TypePredicate<Right<R>> {
+    return function isRightR(value: unknown): value is Right<R> {
+        return svt.isInterface<Right<R>>(value, {type: "Right", data: isR});
+    };
+}
+
+export function validateEither<L, R>(validateL: svt.Validator<L>, validateR: svt.Validator<R>): svt.Validator<Either<L, R>> {
+    return function validateEitherLR(value: unknown): svt.ValidationResult<Either<L, R>> {
+        return svt.validateOneOf<Either<L, R>>(value, [validateLeft(validateL), validateRight(validateR)]);
+    };
+}
+
+export function validateLeft<L>(validateL: svt.Validator<L>): svt.Validator<Left<L>> {
+    return function validateLeftL(value: unknown): svt.ValidationResult<Left<L>> {
+        return svt.validate<Left<L>>(value, {type: "Left", data: validateL});
+    };
+}
+
+export function validateRight<R>(validateR: svt.Validator<R>): svt.Validator<Right<R>> {
+    return function validateRightR(value: unknown): svt.ValidationResult<Right<R>> {
+        return svt.validate<Right<R>>(value, {type: "Right", data: validateR});
+    };
 }
