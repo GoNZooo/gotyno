30b8e4a1bed3cf9980c957c3139511e9165d493c
Rickard Andersson <gonz@severnatazvezda.com>
2020-12-17 22:58:47 +0200

    change link script to warn about MacOS

--- npm-package/scripts/linkForPlatform.js
+++ npm-package/scripts/linkForPlatform.js
@@ -1,8 +1,19 @@
 var process = require("process");
 var fs = require("fs");
 
-const maybeExtension = process.platform === "win32" ? ".exe" : "";
-const BIN_PATH = `./bin/gotyno${maybeExtension}`;
+if (process.platform !== "darwin") {
+  const maybeExtension = process.platform === "win32" ? ".exe" : "";
+  const BIN_PATH = `./bin/gotyno${maybeExtension}`;
 
-fs.symlinkSync(`${process.platform}/gotyno${maybeExtension}`, BIN_PATH);
-fs.chmodSync(BIN_PATH, 0o755);
+  fs.symlinkSync(`${process.platform}/gotyno${maybeExtension}`, BIN_PATH);
+  fs.chmodSync(BIN_PATH, 0o755);
+} else {
+  console.warn("Unfortunately MacOS users currently need to compile `gotyno` themselves.");
+  console.warn(
+    "In the future cross-compilation from Linux/Windows for MacOS will be solved, but currently I'm unable to compile for MacOS."
+  );
+  console.warn(
+    "The repository can be found at https://github.com/GoNZooo/gotyno and the Zig compiler can be found at https://ziglang.org/download/"
+  );
+  console.warn("I may look for someone who can help me with this.");
+}
